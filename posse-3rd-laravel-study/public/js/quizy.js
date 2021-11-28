2cde3dfa9317b3967bdadedf78a641991a53b82b
Haruka Miki <haru.m1k1@keio.jp>
2021-11-28 15:19:36 +0900

    quizy接続完了

---
+++ posse-3rd-laravel-study/public/js/quizy.js
@@ -0,0 +1,70 @@
+"use strict";
+let contentsBox = document.getElementById('contents');
+
+console.log("できてるよ");
+
+let choices = Array();
+choices.push(["たかなわ", "たかわ", "こうわ"]);
+choices.push(["かめいど", "かめど", "かめと"]);
+choices.push(["たかなわ", "たかわ", "こうわ"]);
+choices.push(["かめいど", "かめと", "かめど"]);
+choices.push(["こうじまち", "おかとまち", "かゆまち"]);
+choices.push(["おなりもん", "おかどもん", "ごせいもん"]);
+choices.push(["とどろき", "たたりき", "たたら"]);
+choices.push(["しゃくじい", "せきこうい", "いじい"]);
+choices.push(["ぞうしき", "ざっしょく", "ざっしき"]);
+choices.push(["おかちまち", "みとちょう", "ごしろちょう"]);
+choices.push(["ししぼね", "ろっこつ", "しこね"]);
+choices.push(["こぐれ", "こばく", "こしゃく"]);
+
+let images = Array();
+images.push('https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png');
+images.push('https://d1khcm40x1j0f.cloudfront.net/quiz/512b8146e7661821c45dbb8fefedf731.png');
+
+for (let i = 0; i < choices.length; i++) {
+    let h =  `<div class="question" id="question${i+1}">`
+        + `<h2>${i + 1}.この地名はなんて読む？</h2>`
+        + `<img src="${images[i]}" alt="高輪photo">`
+        + '<ul>'
+        + ` <li id="choice${i}-0" name="choicesName" onclick="onclickFunction(${i},0)">${choices[i][0]}</li>`
+        + `<li id="choice${i}-1" name="choicesName" onclick="onclickFunction(${i},1)">${choices[i][1]}</li>`
+        + `<li id="choice${i}-2" name="choicesName" onclick="onclickFunction(${i},2)">${choices[i][2]}</li>`
+        + '</ul>'
+        + `<div id="answerBox${i}" class="answerBox">`
+        + `<div id="answerBox${i}" class="answerBox">`
+        + `<p id="answerTitle${i}" class="answerTitle"></p>`
+        + `<p id="answerExplanation${i}" class="answerExplanation">正解は「${choices[i][0]}」です！</p>`
+        + '</div>'
+        + '</div>';
+
+    document.write(h);
+    document.getElementById(`answerExplanation${i}`).style.display = 'none';
+};
+
+
+var onclickFunction = function (question, clicked) {
+    let clickedOption = document.getElementById('choice' + question + '-' + clicked);
+    let answerOption = document.getElementById('choice' + question + '-0');
+    let answerBox = document.getElementById('answerBox' + question);
+    let answerTitle = document.getElementById('answerTitle' + question);
+   
+    for (let t=0; t<3; t++){
+    document.getElementById('choice' + question + `-${t}`).style.pointerEvents ="none"; 
+}
+    
+    document.getElementById('answerExplanation'+question).style.display = 'block';
+    answerBox.style.display = 'block';
+    
+    clickedOption.className= 'wrongAnswer';
+    answerOption.className= 'rightAnswer';
+
+    if(clickedOption == answerOption){
+        console.log('反応してるよ');
+        answerTitle.innerText = '正解！';
+        answerTitle.className = 'rightUnderbar';
+    }else{
+        console.log('反応してないよ');
+        answerTitle.innerText = '不正解！';
+        answerTitle.className = 'wrongUnderbar';
+    }
+}
--- posse-3rd-laravel-study/routes/web.php
+++ posse-3rd-laravel-study/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get(‘/quizy‘,‘QuizyController@fshowpage‘);
+Route::get('/quizy','QuizyController@showpage');
