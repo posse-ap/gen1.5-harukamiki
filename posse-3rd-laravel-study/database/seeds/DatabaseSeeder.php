de3efb08ca157437c2351d746b284dcd8e57d69b
Haruka Miki <haru.m1k1@keio.jp>
2021-12-01 21:47:52 +0900

    東京DB設定完了

--- posse-3rd-laravel-study/database/seeds/DatabaseSeeder.php
+++ posse-3rd-laravel-study/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(QuizyTokyoTableSeeder::class);
     }
 }
+
+// seederを登録したいが登録にエラーでる
