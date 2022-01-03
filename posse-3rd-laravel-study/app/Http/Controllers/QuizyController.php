4d0eb0d2dcb70e734d18011379d2d17f8cb403a1
Haruka Miki <haru.m1k1@keio.jp>
2022-01-03 22:29:18 +0900

    dbから表示完了、 写真ファイル追加

--- posse-3rd-laravel-study/app/Http/Controllers/QuizyController.php
+++ posse-3rd-laravel-study/app/Http/Controllers/QuizyController.php
@@ -2,7 +2,7 @@
  
 namespace App\Http\Controllers;
 
-use App\Quizy;
+use App\Question;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
@@ -11,10 +11,11 @@ use QuizyTableSeeder;
 class QuizyController extends Controller
 {
   public function index(Request $request) {
+    $area_id = $request->id;
 
-    // return view('quizy', ['items' => $items]);
-    return view('quizy');
+    $question_choices = Question::all();
+    $question_choices = $question_choices->where('area', '==', $area_id);
+    // dd($question_choice);
+    return view('users.quizy', compact('question_choices'));
   }
-
-
 }
---
+++ posse-3rd-laravel-study/resources/views/users/quizy.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Laravel Quizy</title>
+</head>
+
+<body>
+    @section('title', 'quizy')
+
+    @section('content')
+    <h1>ガチで東京の人しか解けない！＃東京の難読地名クイズ</h1> 
+
+    @foreach ($question_choices as $question_choice)
+
+    <div class="question" id="question{{ $question_choice->id}}">
+        <h2>{{ $question_choice->id}}.この地名はなんて読む？</h2>
+        <img src="{{ asset('image/image' . $question_choice->id) . '.png'}}" alt="image{{$question_choice->id}}">
+        <ul>
+            <li id="choice{{ $question_choice->id}}-0" name="choicesName" onclick="onclickFunction(${i},0)">{{ $question_choice->choice1}}</li>
+            <li id="choice{{ $question_choice->id}}-1" name="choicesName" onclick="onclickFunction(${i},1)">{{ $question_choice->choice2}}</li>
+            <li id="choice{{ $question_choice->id}}-2" name="choicesName" onclick="onclickFunction(${i},2)">{{ $question_choice->choice3}}</li>
+        </ul>
+        <div id="answerBox{{ $question_choice->id}}" class="answerBox">
+            <p id="answerTitle{{ $question_choice->id}}" class="answerTitle"></p>
+            <p id="answerExplanation{{ $question_choice->id}}" class="answerExplanation">正解は「{{ $question_choice->choice1}}」です！</p>
+        </div>
+    </div>
+        @endforeach
+        
+      {{-- <script src="{{ asset('/js/quizy.js') }}"></script> --}}
+
+</body>
+
+</html>
