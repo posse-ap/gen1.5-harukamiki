51976f49b278ad34423e51a5045dd7a403dd0be0
Haruka Miki <haru.m1k1@keio.jp>
2022-03-19 14:38:00 +0900

    migration成功

--- template-3rd-study/routes/web.php
+++ template-3rd-study/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('index', function () {
+Route::get('/index', function () {
     return view('index');
 });
